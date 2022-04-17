244dfdcdf0dee9764401421305250810cd0d2201
Mengxi He <mengxihex@gmail.com>
2022-04-18 00:44:02 +0200

    made grid

---
+++ script.js
@@ -0,0 +1,21 @@
+function buildGrid(x, cellSize, gridElement) {
+    gridElement.style.display = "grid";
+    gridElement.style.gridTemplateColumns = `repeat(${x}, ${cellSize}px)`;
+    gridElement.style.gridTemplateRows = `repeat(${x}, ${cellSize}px)`;
+   
+    let squares = new DocumentFragment();
+  
+    for (let i = 0; i < x * x; i++) {
+      let square = document.createElement('div');
+      square.className = 'square';
+      squares.appendChild(square);
+    }
+  
+    gridElement.appendChild(squares);
+  }
+  
+  let grid = document.querySelector('.grid')
+
+  
+  buildGrid(16, 25, grid);
+  
