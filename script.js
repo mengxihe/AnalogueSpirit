80e1392a1a665db16931e97cfe0a751177b2a2bb
Mengxi He <mengxihex@gmail.com>
2022-04-19 13:28:09 +0200

    finished color mode, rainbow mode, and eraser;

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+const DEFAULT_MODE = 'color';
+const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = '#333333'
 
+let currentMode = DEFAULT_MODE;
+let currentColor = DEFAULT_COLOR;
 
 function buildGrid(x) {
     grid.style.display = "grid";
@@ -6,24 +11,47 @@ function buildGrid(x) {
     grid.style.gridTemplateRows = `repeat(${x}, 1fr)`;
 
     for (let i = 0; i < x * x; i++) {
-      let square = document.createElement('div');
-      square.classList.add('square');
-      square.addEventListener('mousedown', changeColor)
-      square.addEventListener('mouseover', changeColor)
-      grid.appendChild(square);
+        let square = document.createElement('div');
+        square.classList.add('square');
+        square.addEventListener('mousedown', changeColor)
+        square.addEventListener('mouseover', changeColor)
+        grid.appendChild(square);
     }
-  }
+}
 
 function changeColor(e) {
     // console.log(e)
     if (e.type === 'mouseover' && !mouseDown) return;
-    this.classList.add('black');
+    if(currentMode === 'color'){
+        e.target.style.backgroundColor = currentColor;
+    }
+    else if(currentMode === 'erase'){
+        e.target.style.backgroundColor = '#fefefe';
+    }
+    else if(currentMode === 'rainbow'){
+        // this.classList.remove('black');
+        let randomR = Math.floor(Math.random()*256);
+        let randomG = Math.floor(Math.random()*256);
+        let randomB = Math.floor(Math.random()*256);
+        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+
+    }
+    // console.log(this.classList);
 }
 
 
 function reload (){
     grid.innerHTML = ''
     buildGrid(slider.value);
+    setCurrentMode ('color');
+}
+
+function setCurrentMode (newMode) {
+    currentMode = newMode;
+}
+
+function setCurrentColor(newColor) {
+    currentColor = newColor;
 }
 
 const grid = document.querySelector('.container');
@@ -44,5 +72,20 @@ slider.oninput = function(){
     reload();
 }
 buildGrid(slider.value);
-const bw = document.querySelector('.bw');
-const eraser = document.querySelector('.eraser');
+const colorBtn = document.querySelector('.colorBtn');
+const eraserBtn = document.querySelector('.eraser');
+
+colorBtn.onclick = () => setCurrentMode('color');
+eraserBtn.onclick = () => setCurrentMode('erase');
+
+const rainbowBtn = document.querySelector('.rainbowBtn');
+rainbowBtn.onclick = () => setCurrentMode('rainbow');
+
+const colorPicker = document.getElementById ('colorPicker')
+// const colorValue = colorPicker.value;
+// console.log(colorValue);
+colorPicker.oninput = function(){
+    let colorValue = this.value;
+    setCurrentColor(colorValue);
+    // console.log(colorValue);
+}
