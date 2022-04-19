3cd2a22c143d0e0569800061495b20758a7b869d
Mengxi He <mengxihex@gmail.com>
2022-04-19 19:00:14 +0200

    finished all the basic functionalities.

--- script.js
+++ script.js
@@ -15,12 +15,22 @@ function buildGrid(x) {
         square.classList.add('square');
         square.addEventListener('mousedown', changeColor)
         square.addEventListener('mouseover', changeColor)
+        square.style = 'background-color: rgba(255, 255, 255, 1)'
         grid.appendChild(square);
+        square.dataset.darken = 0;
+        
+        // let click = 0;
     }
 }
 
+function clickCount() {
+    click += 1;
+    console.log(click)
+}
+
 function changeColor(e) {
-    // console.log(e)
+    // console.log(e.target.style.backgroundColor);
+    
     if (e.type === 'mouseover' && !mouseDown) return;
     if(currentMode === 'color'){
         e.target.style.backgroundColor = currentColor;
@@ -34,11 +44,51 @@ function changeColor(e) {
         let randomG = Math.floor(Math.random()*256);
         let randomB = Math.floor(Math.random()*256);
         e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
-
+    }
+    else if(currentMode === 'shade'){
+        let oldColor = e.target.style.backgroundColor;
+        // console.log(oldColor);
+        let rgbaString = (oldColor.charAt(3) == 'a') ? oldColor.slice(5, -1) : oldColor.slice(4, -1);
+        let rgbaArray = rgbaString.split(',');
+        let red = rgbaArray[0];
+        let green = rgbaArray[1];
+        let blue = rgbaArray[2];
+        let alpha = rgbaArray[3] ? rgbaArray[3] : 1;
+        let currentOpacity = Number(this.style.backgroundColor.slice(-4, -1));
+        // console.log(alpha)
+        // console.log(currentOpacity)
+        
+        let currentDarkeningStep = e.target.dataset.darken;
+        
+        if (currentDarkeningStep === 9) {
+            this.style.backgroundColor = 'rgba(0,0,0,1)';
+        }
+        let newRed = getNewColorValue(red, currentDarkeningStep, false);
+        let newGreen = getNewColorValue(green, currentDarkeningStep, false);
+        let newBlue = getNewColorValue(blue, currentDarkeningStep, false);
+        let newAlpha = getNewColorValue(currentOpacity, currentDarkeningStep, true);
+        currentDarkeningStep ++
+        e.target.dataset.darken = currentDarkeningStep;
+        
+        this.style.backgroundColor = `rgba(${newRed}, ${newGreen}, ${newBlue}, ${newAlpha})`
+        
+        } 
     }
     // console.log(this.classList);
-}
 
+
+function getNewColorValue(currentColorValue, step, alpha) {
+    let increment;
+    let newValue;
+    if(!alpha) {
+        increment = currentColorValue / (10 - step);
+        newValue = currentColorValue - increment;
+    }else {
+        increment = (1 - currentColorValue) / (10 - step);
+        newValue = +currentColorValue + increment; 
+    }
+    return (newValue);
+    }
 
 function reload (){
     grid.innerHTML = ''
@@ -63,6 +113,9 @@ let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
+let click = 0;
+
+
 const slider = document.getElementById('slider');
 const sliderValue = document.getElementById('sliderValue');
 sliderValue.innerHTML = `${slider.value} x ${slider.value}`;
@@ -71,21 +124,24 @@ slider.oninput = function(){
     sliderValue.innerHTML = `${inner} x ${inner}`;
     reload();
 }
+
 buildGrid(slider.value);
 const colorBtn = document.querySelector('.colorBtn');
 const eraserBtn = document.querySelector('.eraser');
-
 colorBtn.onclick = () => setCurrentMode('color');
 eraserBtn.onclick = () => setCurrentMode('erase');
 
 const rainbowBtn = document.querySelector('.rainbowBtn');
 rainbowBtn.onclick = () => setCurrentMode('rainbow');
 
-const colorPicker = document.getElementById ('colorPicker')
-// const colorValue = colorPicker.value;
-// console.log(colorValue);
+
+const colorPicker = document.getElementById ('colorPicker');
 colorPicker.oninput = function(){
     let colorValue = this.value;
     setCurrentColor(colorValue);
-    // console.log(colorValue);
 }
+
+const shaderBtn = document.querySelector('.shader');
+shaderBtn.onclick = () => setCurrentMode('shade');
+
+// let click = 0;
